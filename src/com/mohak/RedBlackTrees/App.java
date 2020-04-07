87ee335d06b53d6d6de4958e62c30ec660aff855
mohak92 <tamhane.mohak@gmail.com>
2020-04-07 10:55:14 -0400

    RedBlack Tree Implementation

--- src/com/mohak/RedBlackTrees/App.java
+++ src/com/mohak/RedBlackTrees/App.java
@@ -3,5 +3,30 @@ package com.mohak.RedBlackTrees;
 public class App {
 	public static void main(String[] args) {
 		
+		Tree redBlackTree = new RedBlackTree();
+		
+		redBlackTree.insert(10);
+		redBlackTree.insert(20);
+		redBlackTree.insert(30);
+		
+		redBlackTree.traverse();
+		
+		System.out.println("\n");
+		Tree redBlackTree2 = new RedBlackTree();
+		
+		redBlackTree2.insert(10);
+		redBlackTree2.insert(5);
+		redBlackTree2.insert(6);
+		
+		redBlackTree2.traverse();
+		
+		System.out.println("\n");
+		Tree redBlackTree3 = new RedBlackTree();
+		
+		redBlackTree3.insert(10);
+		redBlackTree3.insert(15);
+		redBlackTree3.insert(13);
+		
+		redBlackTree3.traverse();
 	}
 }
--- src/com/mohak/RedBlackTrees/RedBlackTree.java
+++ src/com/mohak/RedBlackTrees/RedBlackTree.java
@@ -15,10 +15,54 @@ public class RedBlackTree implements Tree {
 		if(node.getLeftChild() != null)
 			inOrderTraversal(node.getLeftChild());
 		
-		System.out.println(node + " - ");
+		System.out.print(node + " - ");
 		
 		if(node.getRightChild() != null)
 			inOrderTraversal(node.getRightChild());
+	}
+	
+	private void rightRotate(Node node) {
+		System.out.println("Rotating to the right on Node: " + node);
+		
+		Node tempLeftNode = node.getLeftChild();
+		node.setLeftChild(tempLeftNode.getRightChild());
+		
+		if(node.getLeftChild() != null)
+			node.getLeftChild().setParent(node);
+		
+		tempLeftNode.setParent(node.getParent());
+		
+		if(tempLeftNode.getParent() == null)
+			root = tempLeftNode;
+		else if (node == node.getParent().getLeftChild())
+			node.getParent().setLeftChild(tempLeftNode);
+		else
+			node.getParent().setRightChild(tempLeftNode);
+		
+		tempLeftNode.setRightChild(node);
+		node.setParent(tempLeftNode);
+	}
+	
+	private void leftRotate(Node node) {
+		System.out.println("Rotating to the left on Node: " + node);
+		
+		Node tempRightNode = node.getRightChild();
+		node.setRightChild(tempRightNode.getLeftChild());
+		
+		if(node.getRightChild() != null)
+			node.getRightChild().setParent(node);
+		
+		tempRightNode.setParent(node.getParent());
+		
+		if(tempRightNode.getParent() == null)
+			root = tempRightNode;
+		else if (node == node.getParent().getRightChild())
+			node.getParent().setRightChild(tempRightNode);
+		else
+			node.getParent().setLeftChild(tempRightNode);
+		
+		tempRightNode.setLeftChild(node);
+		node.setParent(tempRightNode);
 	}
 
 	@Override
@@ -29,11 +73,74 @@ public class RedBlackTree implements Tree {
 	}
 
 	private void fixViolations(Node node) {
+		Node parentNode = null;
+		Node grandParentNode = null;
+		
+		while(node != root && node.getColor() != NodeColor.BLACK && node.getParent().getColor() == NodeColor.RED) {
+			parentNode = node.getParent();
+			grandParentNode = node.getParent().getParent();
+			
+			if(parentNode == grandParentNode.getLeftChild()) {
+				Node uncle = grandParentNode.getRightChild();
+				
+				if(uncle != null && uncle.getColor() == NodeColor.RED) {
+					grandParentNode.setColor(NodeColor.RED);
+					parentNode.setColor(NodeColor.BLACK);
+					uncle.setColor(NodeColor.BLACK);
+					node = grandParentNode;
+				} else {
+					if(node == parentNode.getRightChild()) {
+						leftRotate(parentNode);
+						node = parentNode;
+						parentNode = node.getParent();
+					}
+					
+					rightRotate(grandParentNode);
+					NodeColor tempColor = parentNode.getColor();
+					parentNode.setColor(grandParentNode.getColor());
+					grandParentNode.setColor(tempColor);
+					node = parentNode;
+				}
+			} else {
+				Node uncle = grandParentNode.getLeftChild();
+				
+				if(uncle != null && uncle.getColor() == NodeColor.RED) {
+					grandParentNode.setColor(NodeColor.RED);
+					parentNode.setColor(NodeColor.BLACK);
+					uncle.setColor(NodeColor.BLACK);
+					node = grandParentNode;
+				} else {
+					if(node == parentNode.getLeftChild()) {
+						rightRotate(parentNode);
+						node = parentNode;
+						parentNode = node.getParent();
+					}
+					
+					leftRotate(grandParentNode);
+					NodeColor tempColor = parentNode.getColor();
+					parentNode.setColor(grandParentNode.getColor());
+					grandParentNode.setColor(tempColor);
+					node = parentNode;
+				}
+			}
+		}
+		
+		if(root.getColor() == NodeColor.RED){
+			root.setColor(NodeColor.BLACK);
+		}
 		
 	}
 
 	private Node insertIntoTree(Node root, Node node) {
 		if(root == null) return node;
+		
+		if(node.getData() < root.getData()) {
+			root.setLeftChild(insertIntoTree(root.getLeftChild(), node));
+			root.getLeftChild().setParent(root);
+		}else if (node.getData() > root.getData()) {
+			root.setRightChild(insertIntoTree(root.getRightChild(), node));
+			root.getRightChild().setParent(root);
+		}
 		
 		return root;
 	}
