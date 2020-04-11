bc543f9c6afdea73d50a1207d40538dec22a2c62
mohak92 <tamhane.mohak@gmail.com>
2020-04-10 23:53:52 -0400

    Dijkstra's Interpreter

---
+++ src/com/mohak/DijkstrasInterpreter/App.java
@@ -0,0 +1,13 @@
+package com.mohak.DijkstrasInterpreter;
+
+public class App {
+
+	public static void main(String[] args) {
+		Algorithm algorithm = new Algorithm();
+
+		algorithm.interpretExpression("( ( 1 + 2 ) * ( 2 + 1 ) )");
+
+		algorithm.result();
+	}
+
+}
