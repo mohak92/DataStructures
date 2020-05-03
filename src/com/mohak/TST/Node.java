40d9445a61c41c1213a49ed16e160cd9e6cb8390
mohak92 <tamhane.mohak@gmail.com>
2020-05-03 10:22:29 -0400

    TST Node class

--- src/com/mohak/TST/Node.java
+++ src/com/mohak/TST/Node.java
@@ -1,5 +1,45 @@
 package com.mohak.TST;
 
 public class Node {
+	
+	private char character;
+	private Node leftNode;
+	private Node middleNode;
+	private Node rightNode;
+	
+	public Node(char character) {
+		this.character = character;
+	}
 
+	public char getCharacter() {
+		return character;
+	}
+
+	public void setCharacter(char character) {
+		this.character = character;
+	}
+
+	public Node getLeftNode() {
+		return leftNode;
+	}
+
+	public void setLeftNode(Node leftNode) {
+		this.leftNode = leftNode;
+	}
+
+	public Node getMiddleNode() {
+		return middleNode;
+	}
+
+	public void setMiddleNode(Node middleNode) {
+		this.middleNode = middleNode;
+	}
+
+	public Node getRightNode() {
+		return rightNode;
+	}
+
+	public void setRightNode(Node rightNode) {
+		this.rightNode = rightNode;
+	}
 }
